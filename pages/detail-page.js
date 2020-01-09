6989b0587d789d871518afe31c8147c66128aa19
learnerdilip <diliptalks@gmail.com>
2020-01-09 16:14:41 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,15 @@
 function submitComment() {
-    console.log("Hello!")
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    h3.innerText = `${name} said:`
+    p.innerText = msg
+    comment.classList.add('comment') // adding 'comment' variable to class attribute
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    console.log(comment)
 }
